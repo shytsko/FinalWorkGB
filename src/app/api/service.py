2eab914d765e6c45550b6b21abdf8e638d07f920
Vladimir Shytsko <shytsko.v@gmail.com>
2023-07-26 16:55:52 +0300

    Update db create

--- src/app/api/service.py
+++ src/app/api/service.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
-from db.session import init_models
-from db.models import User
+from fastapi import APIRouter, Depends
+from db.session import init_models, get_session
+from db.models import User, UserRole
+from sqlalchemy.ext.asyncio import AsyncSession
 
 service_router = APIRouter()
 
@@ -15,6 +16,11 @@ async def pong():
     return {"ping": "pong!"}
 
 
-@service_router.get("/init-db")
-async def init_db():
+@service_router.post("/init-db")
+async def init_db(db_session: AsyncSession = Depends(get_session)):
     await init_models()
+    superadmin = User(first_name="superadmin", last_name="superadmin", email="superadmin@mail.com",
+                      hashed_password="12345", roles=[role.value for role in UserRole])
+
+    db_session.add(superadmin)
+    await db_session.flush()
--- src/app/db/models.py
+++ src/app/db/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.dialects.postgresql import UUID, ARRAY
 from db.session import Base
 
 
-class PortalRole(str, Enum):
+class UserRole(str, Enum):
     ROLE_USER = "ROLE_USER"
     ROLE_ADMIN = "ROLE_PORTAL_ADMIN"
     ROLE_SUPERADMIN = "ROLE_SUPERADMIN"
@@ -16,8 +16,8 @@ class User(Base):
     __tablename__ = "users"
 
     user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name = Column(String, nullable=False)
-    surname = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     is_active = Column(Boolean(), default=True)
     hashed_password = Column(String, nullable=False)
